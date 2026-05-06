dd084faf4fb4b193220f1cfe29d7fd031f189abf
espenviigsyversen-droid <espen.viig.syversen@gmail.com>
2026-05-06 19:53:52 +0200

    Add files via upload

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ import { initializeApp } from 'https://www.gstatic.com/firebasejs/10.12.2/fireba
     import { getFirestore, doc, collection, getDoc, getDocs, setDoc, deleteDoc, writeBatch }
       from 'https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js';
 
-    const APP_VERSION = 'v14';
+    const APP_VERSION = 'v15';
 
     const firebaseConfig = {
       apiKey: "AIzaSyAMPfQ9gX9rbuvcPsVjYVtq5IT_orjDBPs",
--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "treningsapp-v14";
+const CACHE_NAME = "treningsapp-v15";
 const APP_SHELL = [
   "./",
   "./index.html",
