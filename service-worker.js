9b5ecd45b43241845539cbb0af1e0b0b4e7e0f4e
espenviigsyversen-droid <espen.viig.syversen@gmail.com>
2026-07-16 22:03:25 +0200

    v160g: explain fitness level progress (service-worker.js)

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "treningsapp-v160f";
+const CACHE_NAME = "treningsapp-v160g";
 const FIREBASE_MODULES = [
   "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js",
   "https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js",
