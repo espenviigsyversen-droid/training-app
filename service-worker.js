180169927ee36bf66af5734df5050b0b180048e4
espenviigsyversen-droid <espen.viig.syversen@gmail.com>
2026-07-16 14:23:11 +0200

    v159e: improve AI chat workspace

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "treningsapp-v159d";
+const CACHE_NAME = "treningsapp-v159e";
 const FIREBASE_MODULES = [
   "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js",
   "https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js",
@@ -112,3 +112,4 @@ self.addEventListener("fetch", (event) => {
     )
   );
 });
+
