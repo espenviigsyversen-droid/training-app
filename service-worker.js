5386c316f000d6c8842fae6fdfb3e76de8c286f4
espenviigsyversen-droid <espen.viig.syversen@gmail.com>
2026-07-17 12:19:49 +0200

    v163b: start AI chat with a new conversation

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "treningsapp-v163";
+const CACHE_NAME = "treningsapp-v163b";
 const FIREBASE_MODULES = [
   "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js",
   "https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js",
@@ -114,3 +114,4 @@ self.addEventListener("fetch", (event) => {
     )
   );
 });
+
