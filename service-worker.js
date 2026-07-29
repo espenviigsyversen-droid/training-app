976986a5588ae602ae38ef9c81f89d15bb394863
espenviigsyversen-droid <espen.viig.syversen@gmail.com>
2026-07-29 14:10:49 +0200

    Activate treningsapp-v172d cache

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "treningsapp-v172c";
+const CACHE_NAME = "treningsapp-v172d";
 const FIREBASE_MODULES = [
   "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js",
   "https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js",
