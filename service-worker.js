ad9ae51a5b2c7dc63381cd0b2b0397d662e11ac9
espenviigsyversen-droid <espen.viig.syversen@gmail.com>
2026-08-04 22:17:53 +0200

    v175: activate PWA cache

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "treningsapp-v174c";
+const CACHE_NAME = "treningsapp-v175";
 const FIREBASE_MODULES = [
   "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js",
   "https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js",
@@ -140,3 +140,4 @@ self.addEventListener("fetch", (event) => {
     )
   );
 });
+
