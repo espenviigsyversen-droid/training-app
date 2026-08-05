7717cdf5b8d33d4a59a2cbfa61a45d3cd8a6f64d
espenviigsyversen-droid <espen.viig.syversen@gmail.com>
2026-08-05 20:44:58 +0200

    v176d1: fjern dobbel scrollbar i øktdetalj

--- app.js
+++ app.js
@@ -142,7 +142,7 @@ import { initializeApp } from 'https://www.gstatic.com/firebasejs/10.12.2/fireba
       xWorkoutSuggestion
     } from './domain-training-plan.js';
 
-const APP_VERSION = 'v176d';
+const APP_VERSION = 'v176d1';
     const APP_CACHE_NAME = `treningsapp-${APP_VERSION}`;
 
     const firebaseConfig = {
