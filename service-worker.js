7709b2bbf61fc073e2476c3fc1b09982e2d7a0c5
espenviigsyversen-droid <espen.viig.syversen@gmail.com>
2026-08-10 10:32:03 +0200

    v176e1: require explicit Garmin import actions

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "treningsapp-v176e";
+const CACHE_NAME = "treningsapp-v176e1";
 const FIREBASE_MODULES = [
   "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js",
   "https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js",
