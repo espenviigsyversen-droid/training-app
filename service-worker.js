95c8e34ca3142b894a8ca2e028311f07934554e6
espenviigsyversen-droid <espen.viig.syversen@gmail.com>
2026-08-10 19:31:05 +0200

    Publish v176l2 comparable effort diagnostics (service-worker.js)

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "treningsapp-v176l";
+const CACHE_NAME = "treningsapp-v176l2";
 const FIREBASE_MODULES = [
   "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js",
   "https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js",
