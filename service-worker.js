ff626881d1c30c41b0d978ecc1508e85882245b1
espenviigsyversen-droid <espen.viig.syversen@gmail.com>
2026-08-16 19:54:03 +0200

    v176q: refresh PWA cache

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "treningsapp-v176p";
+const CACHE_NAME = "treningsapp-v176q";
 const FIREBASE_MODULES = [
   "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js",
   "https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js",
@@ -167,4 +167,3 @@ self.addEventListener("fetch", (event) => {
     )
   );
 });
-
