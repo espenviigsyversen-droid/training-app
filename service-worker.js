6482e48fb625d1f0e2d61ba8dffae33454bb9819
espenviigsyversen-droid <espen.viig.syversen@gmail.com>
2026-08-16 11:38:57 +0200

    feat: bygg periodisert blokkdomene v176p

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "treningsapp-v176o1";
+const CACHE_NAME = "treningsapp-v176p";
 const FIREBASE_MODULES = [
   "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js",
   "https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js",
@@ -167,3 +167,4 @@ self.addEventListener("fetch", (event) => {
     )
   );
 });
+
