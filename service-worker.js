814e45826a7f249e346767bb93924ebc9cac926a
espenviigsyversen-droid <espen.viig.syversen@gmail.com>
2026-08-17 12:13:26 +0200

    Publish v176r role model: service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "treningsapp-v176q";
+const CACHE_NAME = "treningsapp-v176r";
 const FIREBASE_MODULES = [
   "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js",
   "https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js",
