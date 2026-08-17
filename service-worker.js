3f3d01229d6031d1a060e0311187313ede5e71ed
espenviigsyversen-droid <espen.viig.syversen@gmail.com>
2026-08-17 15:53:56 +0200

    v176u1: service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "treningsapp-v176u";
+const CACHE_NAME = "treningsapp-v176u1";
 const FIREBASE_MODULES = [
   "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js",
   "https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js",
